ce657870750562a1585f2185279b4a4cc08e2721
csAfter40 <csafter40@gmail.com>
2022-03-31 16:44:32 +0300

    Edited ex2

--- ex2/app.py
+++ ex2/app.py
@@ -1,20 +1,13 @@
+import urllib
+
 def greet(name):
     string = f"Hello, {name}"
     return bytes(string, 'utf-8')
-
-def get_name(environ):
-    query = environ['QUERY_STRING']
-    query_items = query.split('=')
-    name = None
-    if query_items[0] == 'name':
-        name = query_items[1]
-    return name
 
 def application(environ, start_response):
     status = '200 OK'
     headers = [('Content-Type', 'text/html; charset=utf8')]
     start_response(status, headers)
-    name = get_name(environ)
-    if name:
-        return [greet(name)]
-    return [greet('there')]
+    GET = urllib.parse.parse_qs(environ['QUERY_STRING'])
+    name = GET.get('name', ['PyCon'])[0]
+    return [greet(name)]
