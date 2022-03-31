a277171aebb357d12f0bb8ca6ca9d1e87c203d9f
csAfter40 <csafter40@gmail.com>
2022-03-31 16:18:22 +0300

    Edited ex2

--- ex2/app.py
+++ ex2/app.py
@@ -2,15 +2,19 @@ def greet(name):
     string = f"Hello, {name}"
     return bytes(string, 'utf-8')
 
-def application(environ, start_response):
+def get_name(environ):
     query = environ['QUERY_STRING']
     query_items = query.split('=')
     name = None
     if query_items[0] == 'name':
         name = query_items[1]
+    return name
+
+def application(environ, start_response):
     status = '200 OK'
     headers = [('Content-Type', 'text/html; charset=utf8')]
     start_response(status, headers)
+    name = get_name(environ)
     if name:
         return [greet(name)]
     return [greet('there')]
