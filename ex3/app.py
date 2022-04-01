7428e589446390f879092570bcd1ebf3c50da8b7
csAfter40 <csafter40@gmail.com>
2022-04-01 12:19:50 +0300

    Edited ex3

--- ex3/app.py
+++ ex3/app.py
@@ -5,11 +5,7 @@ from decorators import request_response_application
 @request_response_application
 def application(request):
     name = request.GET.get('name', ['PyCon'])[0]
-    # name = request.GET['name'][0]
     context = {
         'name': name
     }
-    response = Response(request, context, greet_template)
-    print("printing rendered string")
-    print(response.render())
-    return [response.render()]
+    return Response(request, context, greet_template)
--- ex3/http_objects.py
+++ ex3/http_objects.py
@@ -11,6 +11,7 @@ class Request:
         return get
 
 class Response:
+    """Response class needs to be iterable in order to be processed by wsgi"""
     status = '200 OK'
     headers = [('Content-Type', 'text/html; charset=utf8')]
 
@@ -18,8 +19,17 @@ class Response:
         self.request = request
         self.context = context
         self.template = template
+        self.used = False
         self.request.start_response(self.status, self.headers)
+
+    def __iter__(self):
+        return self
         
+    def __next__(self):
+        if not self.used:
+            self.used = True
+            return self.render()
+        raise StopIteration
     
     def render(self):
         return self.template(self.context)
